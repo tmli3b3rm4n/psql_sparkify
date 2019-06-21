57df75d468fe81b15c1411e838ba3c22611fdb2d
Daniel Miller <danielmiller5791@gmail.com>
2019-06-21 08:34:09 +0100

    moving forward with normalized and denormalized tables

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,37 +1,39 @@
 CREATE TABLE IF NOT EXISTS d_artist(
+	artist_key SERIAL,
 	artist_id TEXT NOT NULL,
 	artist_name TEXT NOT NULL,
 	artist_location TEXT,
 	artist_longitude NUMERIC,
 	artist_latitude NUMERIC,
-	CONSTRAINT d_artist_pk PRIMARY KEY (artist_id, artist_name),
-	UNIQUE(artist_id),
-	UNIQUE(artist_name)
+	CONSTRAINT d_artist_pk PRIMARY KEY (artist_key),
+	UNIQUE(artist_id)
 );
 
 CREATE TABLE IF NOT EXISTS d_song (
+	song_key SERIAL,
 	song_id TEXT NOT NULL, 
 	song_name TEXT NOT NULL,
 	year INT,
 	length INT,
 	artist_id TEXT references d_artist(artist_id),
-	CONSTRAINT d_song_pk PRIMARY KEY (song_id, song_name),
-	UNIQUE (song_id, song_name)
+	CONSTRAINT d_song_pk PRIMARY KEY (song_key),
+	UNIQUE (song_id, artist_key)
 
 );
 
 CREATE TABLE IF NOT EXISTS d_app_user (
+	user_key SERIAL,
 	user_id SERIAL,
 	first_name TEXT,
 	last_name TEXT,
 	gender VARCHAR(1),
 	level TEXT,
-	PRIMARY KEY (user_id),
+	PRIMARY KEY (user_key),
 	UNIQUE (first_name, last_name)
 );
 
 CREATE TABLE IF NOT EXISTS d_timestamp (
-	timestamp_id SERIAL,
+	timestamp_key SERIAL,
 	second INT,
 	minute INT,
 	hour INT,
@@ -45,7 +47,7 @@ CREATE TABLE IF NOT EXISTS d_timestamp (
 );
 
 CREATE TABLE IF NOT EXISTS f_songplay (
-	songplay_id SERIAL,
+	songplay_key SERIAL,
 	start_time BIGINT,
 	user_id INT REFERENCES d_app_user(user_id),
 	level TEXT,
