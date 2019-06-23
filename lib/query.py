67b356218fd9e181aa32da2ca5b188bede10adf6
Daniel Miller <danielmiller5791@gmail.com>
2019-06-23 15:04:53 +0100

    artist and song tables compliant with feedback

--- lib/query.py
+++ lib/query.py
@@ -16,4 +16,4 @@ query['songplay_insert'] = 'insert into f_songplay (start_time,user_id, level,so
 
 query['artist_insert'] = 'insert into d_artist (artist_id, artist_name, artist_location, artist_longitude, artist_latitude) values (%s, %s, %s, %s, %s) on conflict (artist_id, artist_name) do nothing'
 
-query['song_insert'] = 'insert into d_song (song_id, song_name, year, length, artist_id) values (%s, %s, %s, %s, (select artist_id from d_artist where artist_name=%s))on conflict (song_name, song_id) do nothing'
+query['song_insert'] = 'insert into d_song (song_id, song_name, year, length, artist_key) values (%s, %s, %s, %s, (select artist_key from d_artist where artist_name=%s)) on conflict (song_name, song_id) do nothing'
--- sql/create_table.sql
+++ sql/create_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS d_artist(
 	artist_longitude NUMERIC,
 	artist_latitude NUMERIC,
 	CONSTRAINT d_artist_pk PRIMARY KEY (artist_key),
-	UNIQUE(artist_id)
+	UNIQUE(artist_id, artist_name)
 );
 
 CREATE TABLE IF NOT EXISTS d_song (
@@ -15,44 +15,43 @@ CREATE TABLE IF NOT EXISTS d_song (
 	song_name TEXT NOT NULL,
 	year INT,
 	length INT,
-	artist_id TEXT references d_artist(artist_id),
+	artist_key INT references d_artist(artist_key),
 	CONSTRAINT d_song_pk PRIMARY KEY (song_key),
-	UNIQUE (song_id, artist_key)
-
+	UNIQUE (song_id, song_name)
 );
 
-CREATE TABLE IF NOT EXISTS d_app_user (
-	user_key SERIAL,
-	user_id SERIAL,
-	first_name TEXT,
-	last_name TEXT,
-	gender VARCHAR(1),
-	level TEXT,
-	PRIMARY KEY (user_key),
-	UNIQUE (first_name, last_name)
-);
+-- CREATE TABLE IF NOT EXISTS d_app_user (
+-- 	user_key SERIAL,
+-- 	user_id SERIAL,
+-- 	first_name TEXT,
+-- 	last_name TEXT,
+-- 	gender VARCHAR(1),
+-- 	level TEXT,
+-- 	PRIMARY KEY (user_key),
+-- 	UNIQUE (first_name, last_name)
+-- );
 
-CREATE TABLE IF NOT EXISTS d_timestamp (
-	timestamp_key SERIAL,
-	second INT,
-	minute INT,
-	hour INT,
-	day INT,
-	month INT,
-	year INT,
-	weekday BOOL,
-	timestamp BIGINT,
-	user_id INT REFERENCES d_app_user(user_id),
-	PRIMARY KEY (timestamp_id)
-);
+-- CREATE TABLE IF NOT EXISTS d_timestamp (
+-- 	timestamp_key SERIAL,
+-- 	second INT,
+-- 	minute INT,
+-- 	hour INT,
+-- 	day INT,
+-- 	month INT,
+-- 	year INT,
+-- 	weekday BOOL,
+-- 	timestamp BIGINT,
+-- 	user_id INT REFERENCES d_app_user(user_id),
+-- 	PRIMARY KEY (timestamp_key)
+-- );
 
-CREATE TABLE IF NOT EXISTS f_songplay (
-	songplay_key SERIAL,
-	start_time BIGINT,
-	user_id INT REFERENCES d_app_user(user_id),
-	level TEXT,
-	song_id INT REFERENCES d_song(song_id),
-	artist_id INT REFERENCES d_artist(artist_id),
-	session_id INT,
-	PRIMARY KEY (songplay_id)
-);
+-- CREATE TABLE IF NOT EXISTS f_songplay (
+-- 	songplay_key SERIAL,
+-- 	start_time BIGINT,
+-- 	user_id INT REFERENCES d_app_user(user_id),
+-- 	level TEXT,
+-- 	song_id INT REFERENCES d_song(song_id),
+-- 	artist_id INT REFERENCES d_artist(artist_id),
+-- 	session_id INT,
+-- 	PRIMARY KEY (songplay_key)
+-- );
